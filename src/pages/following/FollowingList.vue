aed805b3a7e9a0cb21a7ce79b451a45f91286524
AdoneyAngel <Adoneyangeltj>
2023-11-14 17:22:12 +0000

    CONTINUE: created following page...

---
+++ src/pages/following/FollowingList.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul class="followingList">
+        <li v-for="user in usersData" :key="user">
+            <h3>{{ user.name }}</h3>
+            <p><span>></span> {{ user.mail }}</p>
+        </li>
+    </ul>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    data() {
+        return {
+            usersData: ref([])
+        }
+    },
+    props: {
+        users: Array
+    },
+    created() {
+        console.log(this.users)
+    }
+}
+
+</script>
--- src/pages/following/LayoutPage.vue
+++ src/pages/following/LayoutPage.vue
@@ -1,34 +1,63 @@
 <template>
     <main class="followingPage">
         
-        
+        <header class="followingContainer">
+            <h1>Following</h1>
+            <input type="text" placeholder="Follow new uses">
+        </header>
+
+        <FollowingList :users="following"/>
         
     </main>
 </template>
 
 <script>
+import { getUserData } from '@/API/DB/db';
 import { getData } from '@/API/localStorage';
 import { ref } from 'vue';
+import FollowingList from './FollowingList.vue';
 
 
 export default {
     data() {
         return {
-            userMail: ref("")
-
-        }
+            userMail: ref(""),
+            following: ref([])
+        };
     },
-    created() {
-        const userMail = getData("userMail")
-
-        this.userMail = userMail
-    }
+    async created() {
+        const userMail = getData("userMail");
+        this.userMail = userMail;
+        getUserData(userMail).then(data => {
+            const followedMails = data.following;
+            let followingUsers = [];
+            followedMails.forEach(async (mail) => {
+                let name = await getUserData(mail);
+                name = name.name;
+                const user = {
+                    mail,
+                    name
+                };
+                followingUsers.push(user);
+            });
+            this.following = followingUsers;
+        });
+    },
+    components: { FollowingList }
 }
 
 </script>
 
 <style scoped>
 
-
+.followingPage {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    height: 100dvh;
+    border: 2px solid black;
+    position: absolute;
+}
 
 </style>
